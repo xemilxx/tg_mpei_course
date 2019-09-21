6d1b8a598aab8305793206e784c4cca6a4f8f2e5
xemilxx <54819396+xemilxx@users.noreply.github.com>
2019-09-22 02:27:45 +0300

    description.json
    { 
      "name": "Зиядов Эмиль Акиф оглы",
      "group": "А-13-18",
      "leetcode": "https://leetcode.com/eeeeeemil/"
    }

---
+++ powerofthree.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/power-of-three/
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+         while (n > 1) {   
+         if (n % 3 != 0) return false;
+         n = n/3;
+         } 
+         return (n == 1);
+    }
+};
