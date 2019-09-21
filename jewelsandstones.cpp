fc236030ad5c5ae7f139ffa273ee2e9e360e64a9
xemilxx <54819396+xemilxx@users.noreply.github.com>
2019-09-21 18:57:36 +0300

    description.json
    { 
      "name": "Зиядов Эмиль Акиф оглы",
      "group": "А-13-18",
      "leetcode": "https://leetcode.com/eeeeeemil/"
    }

---
+++ jewelsandstones.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/jewels-and-stones/
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int k = 0;
+        
+        for (int i = 0; i < J.size(); i++)
+            for (int j = 0; j < S.size(); j++)
+                if (J[i] == S[j]) k++;
+        
+        return k;
+    }
+};
