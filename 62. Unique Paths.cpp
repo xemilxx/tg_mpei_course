eb0766b1a0a5dc7838d002ecdcf30c75fb324cff
xemilxx <54819396+xemilxx@users.noreply.github.com>
2019-10-31 01:59:41 +0300

    Create 62. Unique Paths.cpp

---
+++ 62. Unique Paths.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/unique-paths/description/
+
+class Solution {
+public:
+    
+    int uniquePaths(int m, int n) {
+       
+        /*
+        Всего есть (m + n - 2) шагов в каждом пути, (m - 1) из которых вниз  и (n - 1) вправо.
+        Получается, ответом является число сочетаний.
+        */
+        
+        long c = 1;
+        if (m > n) {
+            swap(m, n);
+        }
+        n--;
+        for (size_t i = 1; i < m; i++) {
+            c = c * (n + i) / i;
+        }
+        return c;
+                
+    }
+};
