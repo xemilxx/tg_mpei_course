f9490c232b964d85b4e2740d59078d32b54b44d0
xemilxx <54819396+xemilxx@users.noreply.github.com>
2019-10-31 02:01:17 +0300

    Create 991. Broken Calculator.cpp

---
+++ 991. Broken Calculator.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/broken-calculator/description/
+
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        
+        int result = 0;
+        
+        while (Y > X) {
+            result++;
+            if (Y % 2 == 1) {
+                Y++;
+            } 
+            else {
+                Y /= 2;    
+            }
+        }
+        
+        return result + X - Y;
+        
+    }
+};
